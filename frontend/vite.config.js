9539542d4595188b8bdcd647197218fc505a7785
Anand19987i <anandpandey1765@gmail.com>
2025-04-13 12:19:53 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { copyFileSync } from 'fs'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        copyFileSync('public/_redirects', 'build/_redirects') // <- change from dist to build
-      },
-    },
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // Source path
+          dest: '.' // Destination path in build/ directory
+        }
+      ]
+    })
   ],
   build: {
-    outDir: 'build', // <- VERY IMPORTANT!
+    outDir: 'build', // Ensure build folder is named 'build'
   },
   server: {
     headers: {
-      "Content-Security-Policy":
+      "Content-Security-Policy": 
         "frame-ancestors 'self' https://www.gstatic.com https://www.google.com;",
     },
   },
